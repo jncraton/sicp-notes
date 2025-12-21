c7993a37a48719463c0f3c86db9fb7023654b606
Jon Craton <jncraton@gmail.com>
2025-12-21 17:36:41 -0500

    Add `else` example

--- 1.1.6.cond.scm
+++ 1.1.6.cond.scm
@@ -1,7 +1,16 @@
+; Define abs using 3 predicates and 3 consequent expressions
 (define (abs x)
   (cond ((> x 0) x)
         ((= x 0) 0)
         ((< x 0) (- x))))
+
+(display (abs -3))
+(newline)
+
+; Define abs using else
+(define (abs x)
+  (cond ((< x 0) (- x))
+        (else x)))
 
 (display (abs -3))
 (newline)
