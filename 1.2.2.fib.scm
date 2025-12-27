0f86dc600ecb0c1f0862f4399694516a21be8a76
Jon Craton <jncraton@gmail.com>
2025-12-27 09:54:37 -0500

    Add iterative method

--- 1.2.2.fib.scm
+++ 1.2.2.fib.scm
@@ -1,8 +1,19 @@
-(define (fib n)
+(define (fibtree n)
   (cond ((= n 0) 0)
     ((= n 1) 1)
-    (else (+ (fib (- n 1))
-             (fib (- n 2))))))
+    (else (+ (fibtree (- n 1))
+             (fibtree (- n 2))))))
+
+(display (fibtree 10))
+(newline)
+
+(define (fib n)
+  (define (fib-iter a b count)
+    (if (= count 0)
+        b
+        (fib-iter (+ a b) a (- count 1))))
+
+  (fib-iter 1 0 n))
 
 (display (fib 10))
 (newline)
